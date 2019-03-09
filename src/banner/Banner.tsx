3b137c2a52a262fdc12683d79d2c62ef3060d40b
TiagoMartins <tiago_gobackto80s@hotmail.com>
2019-03-09 11:20:54 +0000

    Set background image inline in component.

--- src/banner/Banner.tsx
+++ src/banner/Banner.tsx
@@ -7,8 +7,8 @@ const logo = require("../images/ordeal-logo2 dark.png");
 class Banner extends React.Component {
   render() {
     return (
-      <div className="BannerContainer">
-        <div className="Banner" />
+      <div className="BannerContainer" style={{backgroundImage: `background`}}>
+        <div className="Banner" style={{backgroundImage: `url(${process.env.PUBLIC_URL}/background.png)`}}/>
         <img src={logo} />
       </div>
     );
